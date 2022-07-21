61b2788c0cb156c056771583ead791f0f23bb9b4
Arnas T <49756303+AarnasS75@users.noreply.github.com>
2022-07-21 10:27:03 +0300

    Unit Select Updates

--- DodgeBall Tactics/Assets/Scripts/UnitActionSystem.cs
+++ DodgeBall Tactics/Assets/Scripts/UnitActionSystem.cs
@@ -1,12 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitMask;
 
+    public event EventHandler OnSelectedUnitChanged;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -23,10 +40,20 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(hit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
